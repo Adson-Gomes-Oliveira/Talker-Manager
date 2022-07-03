81c7a31449be730321c55e96eae65b63cd394365
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-07-03 20:01:16 -0300

    <error> Invalid Character on String
    Invalid character removed for messages of request failure, the invalid character was "/"

--- services/loginServices.js
+++ services/loginServices.js
@@ -1,5 +1,4 @@
 const HTTP_OK_STATUS = 200;
-const HTTP_BAD_REQUEST_STATUS = 400;
 const MIN_PASSWORD_CHARACTERS = 6;
 
 const generateToken = () => {
@@ -25,22 +24,20 @@ const validateLogin = (email, password) => {
 
 const login = (req, res) => {
   const { email, password } = req.body;
-  if (!email) {
-    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo email é obrigatório' });
-  }
-  if (!password) {
-    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo password é obrigatório' });
-  }
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
 
   const validation = validateLogin(email, password);
+
   if (!validation.isEmailValid) {
-    return res.status(HTTP_BAD_REQUEST_STATUS)
-    .json({ message: 'O email deve ter o formato email@email.com' });
+    return res.status(400)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!validation.isPasswordValid) {
-    return res.status(HTTP_BAD_REQUEST_STATUS)
-    .json({ message: 'O password deve ter pelo menos 6 caracteres' });
+    return res.status(400)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+
   res.status(HTTP_OK_STATUS).json(generateToken());
 };
 
